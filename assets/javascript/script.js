7efce1d3294eef71353fe4a8cbd6ea43991544e1
Mariela Velez <Mariela@Marielas-MacBook-Air.local>
2017-07-24 22:59:02 -0500

    changed from http to https in queryURL

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,7 +25,7 @@ function renderButtons() {
 function displayCharacter() {
 
 	var character = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
 		url: queryURL,
